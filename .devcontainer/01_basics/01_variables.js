1b75d994733eb7f02419be10ffd9d7daab66f1b5
lakssshhh <laksh69sharma@gmail.com>
2025-12-18 08:55:08 +0000

    container config file

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144552
+let accountEmail = "laksh@google.com"
+accountPassword = "2122211"
+
+console.log(accountId)
+console.table([accountId, accountEmail,accountPassword])
+/*
+prefer not to use var because of issue in block scope
+*/
